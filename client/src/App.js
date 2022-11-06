8a8faff4855ed4ee8ed28f10cac1c830876f713c
elaforge98 <51786616+elaforge98@users.noreply.github.com>
2022-11-06 17:05:28 -0500

    Add the basic functionnalities of the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,51 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  //useEffect
+  const [currentTemperature, setCurrentTemperature] = useState()
+  const [currentHumidity, setCurrentHumidity] = useState()
+  const [temperaturePoints, setTemperaturePoints] = useState([])
+  const [humidityPoints, setHumidityPoints] = useState([])
+
+  const calculateAverage = (data) => {
+    const sum = data.reduce((a, b) => a + b, 0);
+    return (sum / data.length) || 0;
+  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let temperature = Math.floor(Math.random() * 40)
+      let humidity = Math.floor(Math.random() * 40)
+      setCurrentTemperature(temperature)
+      setTemperaturePoints(temperaturePoints => [...temperaturePoints, temperature])
+      setCurrentHumidity(Math.floor(Math.random() * 40))
+      setHumidityPoints(humidityPoints => [...humidityPoints, humidity])
+      
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className='data-container'>
+        <div className='temperature-container'>
+          <h2>Temperature</h2>
+          <h3>Current Temperature</h3>
+          {currentTemperature}
+          <h3>Average Temperature</h3>
+          {calculateAverage(temperaturePoints).toFixed(2)}
+          <h3>Last 30 Temperature Data Points</h3>
+          {temperaturePoints.slice(-30).map((item, i) => <li key={i}>{item}</li>)}
+        </div>
+        <div className='humidity-container'>
+          <h2>Humidity</h2>
+          <h3>Current Temperature</h3>
+          {currentHumidity}
+          <h3>Average Humidity</h3>
+          {calculateAverage(humidityPoints).toFixed(2)}
+          <h3>Last 30 Humidity Data Points</h3>
+          {humidityPoints.slice(-30).map((item, i) => <li key={i}>{item}</li>)}
+        </div>
+      </div>
     </div>
   );
 }
