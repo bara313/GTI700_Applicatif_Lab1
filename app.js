4f598d8f1c3aa5d9627cefa06738d0af01ab65e3
Justin L <justlussier@gmail.com>
2022-11-06 14:03:14 -0500

    Backend returns JSON

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express');
 var CircularBuffer = require("circular-buffer");
 const app = express();
 const port = 8080;
-var temperatureBuffer=new CircularBuffer(720)
+var temperatureBuffer = new CircularBuffer(720)
 
 const { spawn } = require('node:child_process');
-const ls = spawn('python', ['-u','./temperature.py']);
+const ls = spawn('python', ['-u', './temperature.py']);
 
 ls.stdout.on('data', (data) => {
-  temperatureBuffer.enq(data)
+	temperatureBuffer.enq(json.parse(data))
 });
 
 ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
+	console.error(`stderr: ${data}`);
 });
 
 ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
+	console.log(`child process exited with code ${code}`);
 });
 
 
@@ -25,7 +25,7 @@ ls.on('close', (code) => {
 
 app.get('/', (req, res) => res.send('Hello World!: ' + temp))
 
-app.get('/sensors/temperature', (req, res) => res.send('Hello World!: ' + temperatureBuffer.get(0)))
+app.get('/sensors/temperature', (req, res) => res.send(temperatureBuffer.get(0)))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
